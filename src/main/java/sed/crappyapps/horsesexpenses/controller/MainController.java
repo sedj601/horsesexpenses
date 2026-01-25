484e91dfb228bb388842bc795fd6896848c1263c
Sedrick Jefferson <sedrick.work@gmail.com>
2026-01-24 21:48:43 -0600

    Update 9:48 PM

--- src/main/java/sed/crappyapps/horsesexpenses/controller/MainController.java
+++ src/main/java/sed/crappyapps/horsesexpenses/controller/MainController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package sed.crappyapps.horsesexpenses;
+package sed.crappyapps.horsesexpenses.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
